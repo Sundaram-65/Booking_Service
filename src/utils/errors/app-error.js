45ad2d30d4153f47d9ae7a46feb2db6b227eba4c
Sundaram-65 <sundaramgupta010203@gmail.com>
2026-04-14 16:19:25 +0530

    Added error files

---
+++ src/utils/errors/app-error.js
@@ -0,0 +1,19 @@
+const {StatusCodes}=require('http-status-codes');
+
+class AppError extends Error{
+
+    constructor(
+        name,
+        message='Something went wrong',
+        explanation,
+        statusCodes=StatusCodes.INTERNAL_SERVER_ERROR
+    ){
+        super();
+        this.name=name;
+        this.message=message;
+        this.explanation=explanation;
+        this.statusCodes=statusCodes
+    }
+}
+
+module.exports=AppError
